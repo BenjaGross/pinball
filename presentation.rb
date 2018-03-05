3267d84f2711b8409403d7a1a28684c9f687fbe4
Benjamin Gross <benja.j.gross@gmail.com>
2018-03-05 10:45:04 -0500

    Initial commit

---
+++ presentation.rb
@@ -0,0 +1,11 @@
+require 'slide_hero'
+
+presentation 'pinball' do
+  slide "Presentations made easy!" do
+    point "Make presentations at the speed of thought"
+    point "Make ideas pop", animation: "grow"
+    list do
+      point "All kinds of awesome here"
+    end
+  end
+end
